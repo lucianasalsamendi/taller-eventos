a960d15d23396e284f4e668c6f39c4045f9253df
lucianasalsamendi <lucianasalsamendi@gmail.com>
2023-08-09 04:29:38 -0300

    Alerta Div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botonDiv = document.getElementById("boton"); 
+  
+    botonDiv.addEventListener("click", function() {
+      alert("Hola! Soy el div");
+    });
+  });
+  
+
