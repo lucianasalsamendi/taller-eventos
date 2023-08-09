f6c8d544e216770bd76e2f75dd30b17f6514fb01
lucianasalsamendi <lucianasalsamendi@gmail.com>
2023-08-09 05:29:05 -0300

    solucion eventos

--- eventos.js
+++ eventos.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const botonDiv = document.getElementById("boton"); 
   
+    botonDiv.addEventListener("click", function(event) {
+        event.stopPropagation();
+      });
+
+
+    botonDiv.addEventListener("click", function() {
+    alert("Hola! Soy el botón");
+  });
+
+
     botonDiv.addEventListener("click", function() {
       alert("Hola! Soy el div");
     });
